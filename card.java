71915cb3a165541eef353c05de016dda976159fc
Lambeau <155316702+lambeau7@users.noreply.github.com>
2026-01-28 13:18:31 -0600

    Added stuff to funny card class
    Added properties and methods for card class.

--- card.java
+++ card.java
@@ -1,3 +1,35 @@
 public class card {
+  private int rank;
+  private string suit;
+  private boolean faceUp;
 
+  public card(int rank, string suit)
+  {
+    this.rank = rank;
+    this.suit = suit;
+    this.faceUp = false;
+  }
+
+  public int getRank()
+  {
+    return rank;
+  }
+  public string getSuit()
+  {
+    return suit;
+  }
+  public boolean isFaceUp()
+  {
+    return faceUp;
+  }
+  public void flip()
+  {
+    faceUp = !faceUp;
+  }
+  public string toString()
+  {
+    return( faceUp ? rank + "" + suit : "XX");
+    // HOLY HELL THIS IS AMAZING ^^^ (basically shortened a entire if statement :D
+  }
+  
 }
