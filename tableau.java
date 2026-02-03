7c4653d709bde4413b20aa564aa9b5c04bb43a43
FrostSVG976 <kjanik2009@gmail.com>
2026-02-03 09:17:15 -0600

    more table methods

--- tableau.java
+++ tableau.java
@@ -28,8 +28,55 @@ public class tableau
         }
     }
 
+    public boolean canPlace(card x, int sp)
+    {
+        Stack<card> yes = mycolumns.get(sp);
+        card y = yes.peek();
+        if(x.getRank() == 1)
+        {
+            return false;
+        }
+        if(y.getColor().equals("black"))
+        {
+            if(x.getColor().equals("red") && x.getRank() == y.getRank() - 1)
+            {
+                yes.push(x);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            if(x.getColor().equals("black") && x.getRank() == y.getRank() - 1)
+            {
+                yes.push(x);
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean canCut(int csp, int msp, int msp2)
+    {
+        Stack<card> x = mycolumns.get(msp);
+        //taking a break now, atleast on this method
+        //notes for steps
+        //1. put supposed stack of cards you want to move into a stack(peek)
+        //2. check if you can put stack into new spot in new column(canPlace bottom card)
+        //3. Return false if you cant
+        //4. if you can, create the stack for real real(pop instead of peek)
+        //5. move real stack to new spot
+        // - PAST KAI
+        return false;
+    }
     public boolean newStack(card x, int sp)
     {
+        if(mycolumns.get(sp).empty() == true)
+        {
+            return false;
+        }
         if (x.getRank() == 13)
         {
             mycolumns.get(sp).push(x);
